a9ad76c7435345ad9d239417b842361c41a7aaf3
nzbasic <jcop589@aucklanduni.ac.nz>
2020-10-03 22:20:59 +1300

    error messages

--- src/quinzical/quinzicalExceptions.java
+++ src/quinzical/quinzicalExceptions.java
@@ -1,5 +1,8 @@
 package quinzical;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class quinzicalExceptions extends RuntimeException {
 	/**
 	 * 
@@ -7,12 +10,11 @@ public class quinzicalExceptions extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public quinzicalExceptions(String err) {
-		/**
-		 * Alert errormsg = new Alert(AlertType.ERROR); errormsg.setTitle("Error");
-		 * errormsg.setHeaderText("An error has occured");
-		 * 
-		 * errormsg.setContentText( err + "\nIf the program is not functioning as
-		 * expected, please quit the game and try restarting"); errormsg.showAndWait();
-		 **/
+		
+		Alert errormsg = new Alert(AlertType.ERROR); errormsg.setTitle("Error");
+		errormsg.setHeaderText("An error has occured");
+		 
+		errormsg.setContentText( err + "\nIf the program is not functioning as expected, please quit the game and try restarting"); errormsg.showAndWait();
+		
 	}
 }
