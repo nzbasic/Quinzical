728df640d33e75f6f201546a297606b728ee6128
nzbasic <jcop589@aucklanduni.ac.nz>
2020-10-12 17:37:09 +1300

    add adding question scene

---
+++ src/quinzical/scenes/AddQuestionController.java
@@ -0,0 +1,49 @@
+package quinzical.scenes;
+
+import java.io.IOException;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import quinzical.quinzical;
+import quinzical.model.Category;
+import quinzical.model.CategoryLoader;
+
+public class AddQuestionController {
+    @FXML
+    private ComboBox<Category> categoryBox;
+    @FXML
+    private TextField questionText;
+    @FXML
+    private TextField answerText;
+    @FXML
+    private Label popup;
+
+    @FXML
+    private void initlialize() {
+        CategoryLoader loader = new CategoryLoader();
+        List<Category> list = loader.getCategories();
+        for (Category category : list) {
+            categoryBox.getItems().add(category);
+        }
+    }
+
+    @FXML
+    private void add() {
+        Category category = categoryBox.getValue();
+        String questionString = questionText.getText();
+        String answerString = answerText.getText();
+        if (category == null || questionString == null || answerString == null) {
+            popup.setVisible(true);
+            return;
+        }
+
+    }
+
+    @FXML
+    private void cancel() throws IOException {
+        quinzical.loadFXML("Menu");
+    }
+}
