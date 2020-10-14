6a39b9262eb356ed53663b65aefe55aca07479c6
James <jamescoppard024@gmail.com>
2020-10-14 14:33:33 +1300

    add initialization of combobox

--- src/quinzical/scenes/ShopController.java
+++ src/quinzical/scenes/ShopController.java
@@ -1,7 +1,14 @@
 package quinzical.scenes;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import quinzical.model.Inventory;
+import quinzical.model.InventoryLoader;
 
 public class ShopController {
     @FXML
@@ -15,5 +22,16 @@ public class ShopController {
     @FXML
     private ComboBox<Theme> themeBox;
 
-    
+    @FXML
+    private void initialize() {
+
+        InventoryLoader loader = new InventoryLoader();
+        Inventory inventory = loader.getInventory();
+        List<Theme> themes = inventory.getThemes();
+
+        for (Theme theme : themes) {
+            themeBox.getItems().add(theme);
+        }
+    }
+
 }
