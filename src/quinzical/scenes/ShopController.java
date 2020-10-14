ea563b9b10c56e31d3d0a11415fce4ff6367b3ed
James <jamescoppard024@gmail.com>
2020-10-14 13:17:56 +1300

    add shop base fxml and controller

---
+++ src/quinzical/scenes/ShopController.java
@@ -0,0 +1,19 @@
+package quinzical.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class ShopController {
+    @FXML
+    private void buyHint() {
+
+    }
+    @FXML
+    private void buyTheme() {
+
+    }
+    @FXML
+    private ComboBox<Theme> themeBox;
+
+    
+}
