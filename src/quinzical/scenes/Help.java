5f0aacdb63689f65b1ea7ba72a17c3860e5c78b6
James <jcop589@aucklanduni.ac.nz>
2020-10-18 20:33:22 +1300

    create help screen templates

---
+++ src/quinzical/scenes/Help.java
@@ -0,0 +1,17 @@
+package quinzical.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public abstract class Help {
+    @FXML
+    private AnchorPane helpOverlay;
+    @FXML
+	public void help() {
+		helpOverlay.setVisible(true);
+	}
+	@FXML
+	public void helpDown() {
+		helpOverlay.setVisible(false);
+	}
+}
